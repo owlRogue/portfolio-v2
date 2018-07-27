63e8544f328457390fad2c8b9c241fa81617299b
Koltyn Palmer <palmer@ring.com>
2018-07-27 15:44:39 -0700

    heroku prep

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 // Require dependencies
 var express = require("express");
-var http = require("http");
-var fs = require("fs");
 
 var bodyParser = require("body-parser");
 var path = require("path");
@@ -24,50 +22,23 @@ app.use(express.static("public"));
 // Routes
 // =============================================================
 
-// Basic route that sends the user first to the AJAX Page
+// Basic route that sends the user first to the home Page
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-// Starts the server to begin listening
-// =============================================================
+
+  app.get("/about", function(req, res) {
+    res.sendFile(path.join(__dirname, "about.html"));
+  });
+    app.get("/contact", function(req, res) {
+    res.sendFile(path.join(__dirname, "contact.html"));
+  });
+    app.get("/portfolio", function(req, res) {
+    res.sendFile(path.join(__dirname, "portfolio.html"));
+  });
+
+// // Starts our server.
 app.listen(port, function() {
   console.log("App listening on PORT " + port);
 });
-
-
-// // Set our port to 8080
-// var PORT = 8080;
-
-// var server = http.createServer(handleRequest);
-
-function handleRequest(req, res) {
-
-  // Capture the url the request is made to
-  var path = req.url;
-
-  // When we visit different urls, call the function with different arguments
-  switch (path) {
-
-  case "/about":
-  case "/contact":
-  case "/portfolio":
-    return renderHTML(path + ".html", res);
-
-  default:
-    return renderHTML("/index.html", res);
-  }
-}
-
-// function to take a filepath and respond with html
-function renderHTML(filePath, res) {
-  return fs.readFile(__dirname + filePath, function(err, data) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(data);
-  });
-}
-
-// // Starts our server.
-// server.listen(PORT, function() {
-//   console.log("Server is listening on PORT: " + PORT);
-// });
